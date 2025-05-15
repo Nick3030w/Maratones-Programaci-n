1f2ce735dfaabe3a1cff6b6f83e768a931725388
Nick3030w <niconianessu@gmail.com>
2025-05-14 20:33:22 -0500

    Comienzo 18

---
+++ Nick/src/ejercicio18.java
@@ -0,0 +1,28 @@
+/**
+ * 18. Un centro de investigación ambiental registra la temperatura 4 veces al
+ * día durante los 7
+ * días de una semana. Guarde en un arreglo los registros realizados y
+ * determine:
+ * a) La temperatura promedio de cada día de la semana.
+ * b) La temperatura promedio general de todos los registros realizados.
+ * c) Cantidad de registros inferiores al promedio general.
+ * 
+ * @author Nick3030w
+ */
+public class ejercicio18 {
+    public static void main(String[] args) {
+        int lunes[] = new int[3];
+        int martes[] = new int[3];
+        int miercoles[] = new int[3];
+        int jueves[] = new int[3];
+        int viernes[] = new int[3];
+        int sabado[] = new int[3];
+        int domingo[] = new int[3];
+        lunes[0] = 34;
+        lunes[1] = 21;
+        lunes[2] = 28;
+        martes[0] = 23;
+        miercoles[0] = 30;
+        jueves[0] = 
+    }
+}
